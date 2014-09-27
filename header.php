a056306eb8b7fd2bc946036bf0b4d4930805a813
Hank Sway <hank.sway@gmail.com>
2014-09-27 17:18:42 -0400

    added a missing >

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 	<head>
 		<meta charset="utf-8">
-		<meta name="description" content="Hank Sway - DWA15 P2"
+		<meta name="description" content="Hank Sway - DWA15 P2">
     	<!-- Latest compiled and minified CSS (Bootstrap) -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
