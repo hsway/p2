b191bee6a7fa0242dd8b3ad3625a963b9d94774a
Hank Sway <hank.sway@gmail.com>
2014-09-27 17:13:05 -0400

    fixed validation errors

--- content.php
+++ content.php
@@ -10,16 +10,16 @@
 	<h3>Make a new one:</h3>
 
 	<form method="GET" action="/index.php">
-		<label name="count">Number of words: </label>
-		<input type="text" name="count" name="count" maxlength="1" id="count" value="<?php echo ($count == 1) ? '' : $count ; ?>"> (max = 9)<br />
+		<label for="count">Number of words: </label>
+		<input type="text" name="count" maxlength="1" id="count" value="<?php echo ($count == 1) ? '' : $count ; ?>"> (max = 9)<br />
 		
-		<label name="uppercase">Uppercase first letter? </label>
+		<label for="uppercase">Uppercase first letter? </label>
 		<input type="checkbox" name="uppercase" value="uppercase" <?php echo ($uppercase) ? 'checked="checked"' : '' ; ?>><br />
 		
-		<label name="symbol">Use a symbol? </label>
+		<label for="symbol">Use a symbol? </label>
 		<input type="checkbox" name="symbol" value="symbol" <?php echo ($symbol) ? 'checked="checked"' : '' ; ?>><br />
 		
-		<label name="number">Include a number? </label>
+		<label for="number">Include a number? </label>
 		<input type="checkbox" name="number" value="number" <?php echo ($number) ? 'checked="checked"' : '' ; ?>><br /><br />
 
 		<input type="submit" class="btn btn-primary" value="Get your password!"/>
--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="description" content="Hank Sway - DWA15 P2"
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
     	<!-- Latest compiled and minified CSS (Bootstrap) -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
